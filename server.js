3d997b187c62a75fbc266adeb30596961004bc17
Ian <onyangoian71@gmail.com>
2021-07-15 18:03:36 +0300

    Views set up

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
 
 const originRouter  = require('./routes/origin')
+const indexRouter  = require('./routes/index')
 
 require('./db/db')
 
-app.use(express.json())
-app.get('/', (req,res)=>{
-	res.send('Heloo')
-})
+app.set("view engine" , "ejs")
+app.set("views", __dirname + '/views')
+app.set("layout", "layouts/layout")
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+
 
 app.use('/origin',  originRouter)
+app.use('/',  indexRouter)
 
 const port =process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`app running on ${port}`))
