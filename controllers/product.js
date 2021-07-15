2ce14290af1ea36ba21ad2d817934179af3ecb3c
Ian <onyangoian71@gmail.com>
2021-07-15 22:38:38 +0300

    Add products field

--- controllers/product.js
+++ controllers/product.js
@@ -1,9 +1,17 @@
-const newProduct = (req,res,next) =>{
-    res.render('new_product')
-}
+const Origin = require('../models/origin')
 
 const viewProduct = (req,res,next) =>{
     res.render('view_product')
+}
+
+const newProduct = async (req,res,next) =>{
+
+    try{
+        const originName = await Origin.find({})
+        res.render('new_product', { originName: originName })
+    }catch(err){
+        res.send(err)
+    }
 }
 
 module.exports = {
