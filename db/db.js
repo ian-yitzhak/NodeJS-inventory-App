8f307ee667a3857d58ef524e69d236ec6939d610
Ian <onyangoian71@gmail.com>
2021-07-17 11:45:04 +0300

    prepare deployment

--- db/db.js
+++ db/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(
-	"mongodb+srv://ian_test:METH71@sellah@ian.qfm7u.mongodb.net/auth?retryWrites=true&w=majority",
+	"mongodb+srv://ian_test:<password>.qfm7u.mongodb.net/database?retryWrites=true&w=majority",
 	{ useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true  }
 
 	)
 .then(()=> console.log('Connected successfully'))
 .catch((err)=> console.log(err))
+
+//use the correct configurations for the password and database
